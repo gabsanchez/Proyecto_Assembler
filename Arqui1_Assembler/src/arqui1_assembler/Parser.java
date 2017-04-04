a52adb09919b9cd35225c1ff45b6d017081ce41d
Williams <wil.25_r@yahoo.com>
2017-04-04 13:43:58 -0600

    Intruccion C

--- Arqui1_Assembler/src/arqui1_assembler/Parser.java
+++ Arqui1_Assembler/src/arqui1_assembler/Parser.java
@@ -88,7 +88,30 @@ public final class Parser {
         
     }
     
-    public byte[] comp(String Instruccion)
+    private String InstruccionC(String Instruccion)
+    {
+        String Salida = "";
+        String[] DCJ= new String[2];
+        if (Instruccion.contains("=")) {
+            DCJ = Instruccion.split("=");
+            String Aux="";
+            for (int i = 0; i < 10; i++) {
+                Aux= Aux + comp(DCJ[1])[i];
+            }
+            Salida = Aux+Dest(DCJ[0])+"000";
+        }
+        else if (Instruccion.contains(";")) {
+            DCJ = Instruccion.split(";");
+            String Aux="";
+            for (int i = 0; i < 10; i++) {
+                Aux= Aux + comp(DCJ[0])[i];
+            }
+            Salida = Aux +"000"+Salto(DCJ[1]);
+        }
+        return Salida;
+    }
+    
+    private byte[] comp(String Instruccion)
     {
         byte[] Salida = new byte[10];
         Salida[0] = 1;
@@ -250,6 +273,95 @@ public final class Parser {
                 break;
             case "D|A":
                 Salida[3] = 0;
+                Salida[4] = 0;
+                Salida[5] = 1;
+                Salida[6] = 0;
+                Salida[7] = 1;
+                Salida[8] = 0;
+                Salida[9] = 1;
+            case "M":
+                Salida[3] = 1;
+                Salida[4] = 1;
+                Salida[5] = 1;
+                Salida[6] = 0;
+                Salida[7] = 0;
+                Salida[8] = 0;
+                Salida[9] = 0;
+                break;
+            case "!M":
+                Salida[3] = 1;
+                Salida[4] = 1;
+                Salida[5] = 1;
+                Salida[6] = 0;
+                Salida[7] = 0;
+                Salida[8] = 0;
+                Salida[9] = 1;
+                break;
+            case "-M":
+                Salida[3] = 1;
+                Salida[4] = 1;
+                Salida[5] = 1;
+                Salida[6] = 0;
+                Salida[7] = 0;
+                Salida[8] = 1;
+                Salida[9] = 1;
+                break;
+            case "M+1":
+                Salida[3] = 1;
+                Salida[4] = 1;
+                Salida[5] = 1;
+                Salida[6] = 0;
+                Salida[7] = 1;
+                Salida[8] = 1;
+                Salida[9] = 1;
+                break;
+            case "M-1":
+                Salida[3] = 1;
+                Salida[4] = 1;
+                Salida[5] = 1;
+                Salida[6] = 0;
+                Salida[7] = 0;
+                Salida[8] = 1;
+                Salida[9] = 0;
+                break;
+            case "D+M":
+                Salida[3] = 1;
+                Salida[4] = 0;
+                Salida[5] = 0;
+                Salida[6] = 0;
+                Salida[7] = 0;
+                Salida[8] = 1;
+                Salida[9] = 0;
+                break;
+            case "D-M":
+                Salida[3] = 1;
+                Salida[4] = 0;
+                Salida[5] = 1;
+                Salida[6] = 0;
+                Salida[7] = 0;
+                Salida[8] = 1;
+                Salida[9] = 1;
+                break;
+            case "M-D":
+                Salida[3] = 1;
+                Salida[4] = 0;
+                Salida[5] = 0;
+                Salida[6] = 0;
+                Salida[7] = 1;
+                Salida[8] = 1;
+                Salida[9] = 1;
+                break;
+            case "D&M":
+                Salida[3] = 1;
+                Salida[4] = 0;
+                Salida[5] = 0;
+                Salida[6] = 0;
+                Salida[7] = 0;
+                Salida[8] = 0;
+                Salida[9] = 0;
+                break;
+            case "D|M":
+                Salida[3] = 1;
                 Salida[4] = 0;
                 Salida[5] = 1;
                 Salida[6] = 0;
