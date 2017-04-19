afc15cf701ec3c21469cb528aecb32ff39fadc48
Williams <wil.25_r@yahoo.com>
2017-04-18 21:05:31 -0600

    Push y Pop

--- Arqui1_Assembler/src/arqui1_assembler/VM_Traductor.java
+++ Arqui1_Assembler/src/arqui1_assembler/VM_Traductor.java
@@ -129,83 +129,171 @@ public class VM_Traductor {
         Comandos = Instruccion.substring(Instruccion.indexOf(" "), Instruccion.length()).trim();
         Contenido[1] = Comandos.substring(0, Comandos.indexOf(" "));
         Comandos = Comandos.substring(Comandos.indexOf(" "), Comandos.length()).trim();
-        Contenido[2] = Comandos;
-        if (Contenido[1].equals("constant")) //Obtener el valor del numero y guardarlo en D.
-        {
-            InstruccionesASM.add("@"+Contenido[2]);
-            InstruccionesASM.add("D=A");
-        }
-        else if (Contenido[1].equals("local")||Contenido[1].equals("argument")||Contenido[1].equals("this")||Contenido[1].equals("that")) 
-        { 
-            String PR = "";
+        Contenido[2] = Comandos.trim();
+        if (Contenido[0].equals("push")) {
             switch(Contenido[1])
             {
                 case "local":
-                    PR="LCL";
+                    InstruccionesASM.add("@LCL");
+                    InstruccionesASM.add("D=M");
+                    InstruccionesASM.add("@"+Contenido[2]);
+                    InstruccionesASM.add("A=D+A");
+                    InstruccionesASM.add("D=M");
+                    Push();
                     break;
                 case "argument":
-                    PR="ARG";
+                    InstruccionesASM.add("@ARG");
+                    InstruccionesASM.add("D=M");
+                    InstruccionesASM.add("@"+Contenido[2]);
+                    InstruccionesASM.add("A=D+A");
+                    InstruccionesASM.add("D=M");
+                    Push();
                     break;
                 case "this":
-                    PR="THIS";
+                    InstruccionesASM.add("@THIS");
+                    InstruccionesASM.add("D=M");
+                    InstruccionesASM.add("@"+Contenido[2]);
+                    InstruccionesASM.add("A=D+A");
+                    InstruccionesASM.add("D=M");
+                    Push();
                     break;
                 case "that":
-                    PR="THAT";
+                    InstruccionesASM.add("@THAT");
+                    InstruccionesASM.add("D=M");
+                    InstruccionesASM.add("@"+Contenido[2]);
+                    InstruccionesASM.add("A=D+A");
+                    InstruccionesASM.add("D=M");
+                    Push();
                     break;
-                default:
-                    break;
-            }
-            InstruccionesASM.add("@"+PR);
-            InstruccionesASM.add("D=A");
-            InstruccionesASM.add("@"+Contenido[2]);
-            InstruccionesASM.add("D=D+M");
-        }
-        else if (Contenido[1].equals("pointer")||Contenido[1].equals("temp"))
-        {
-            int Base=0;
-            switch(Contenido[1])
-            {
                 case "pointer":
-                    Base=3;
+                {
+                    if (Contenido[2].equals("0")) {
+                        InstruccionesASM.add("@THIS");
+                        InstruccionesASM.add("D=M");
+                        Push();
+                    }
+                    else { 
+                        InstruccionesASM.add("@THIS");
+                        InstruccionesASM.add("D=M");
+                        Push();
+                    }
+                    break;
+                }
+                case "constant":
+                    InstruccionesASM.add("@"+Contenido[2]);
+                    InstruccionesASM.add("D=A");
+                    Push();
+                    break;
+                case "static":
+                    InstruccionesASM.add("@var"+Contenido[2]);
+                    InstruccionesASM.add("D=M");
+                    Push();
                     break;
                 case "temp":
-                    Base=5;
-                    break;
+                   InstruccionesASM.add("@R5");
+                   InstruccionesASM.add("D=A");
+                   InstruccionesASM.add("@"+Contenido[2]);
+                   InstruccionesASM.add("A=D+A");
+                   InstruccionesASM.add("D=M");
+                   Push();
+                   break;
                 default:
                     break;
             }
-            InstruccionesASM.add("@"+Base);
-            InstruccionesASM.add("D=A");
-            InstruccionesASM.add("@"+Contenido[2]);
-            InstruccionesASM.add("D=D+A");
         }
-        else if (Contenido[1].equals("static")) {
-            InstruccionesASM.add("@var"+Contenido[2]);
-            InstruccionesASM.add("D=M");
-        }
-        
-        if (Contenido[0].equals("push")) 
-        {
-            InstruccionesASM.add("@SP");
-            InstruccionesASM.add("A=M");
-            InstruccionesASM.add("M=D");
-            InstruccionesASM.add("D=A+1");
-            InstruccionesASM.add("@SP");
-            InstruccionesASM.add("M=D");
-        }
-        else if (Contenido[0].equals("pop")) 
-        {
-            InstruccionesASM.add("@R13");
-            InstruccionesASM.add("M=D");
-            InstruccionesASM.add("@SP");
-            InstruccionesASM.add("A=M-1");
-            InstruccionesASM.add("D=M");
-            InstruccionesASM.add("@R13");
-            InstruccionesASM.add("A=M");
-            InstruccionesASM.add("M=D");
+        else if (Contenido[0].equals("pop")) {
+            switch(Contenido[1])
+            {
+                case "local":
+                    InstruccionesASM.add("@LCL");
+                    InstruccionesASM.add("D=M");
+                    InstruccionesASM.add("@"+Contenido[2]);
+                    InstruccionesASM.add("D=D+A");
+                    Push();
+                    break;
+                case "argument":
+                    InstruccionesASM.add("@ARG");
+                    InstruccionesASM.add("D=M");
+                    InstruccionesASM.add("@"+Contenido[2]);
+                    InstruccionesASM.add("A=D+A");
+                    InstruccionesASM.add("D=M");
+                    Push();
+                    break;
+                case "this":
+                    InstruccionesASM.add("@THIS");
+                    InstruccionesASM.add("D=M");
+                    InstruccionesASM.add("@"+Contenido[2]);
+                    InstruccionesASM.add("A=D+A");
+                    InstruccionesASM.add("D=M");
+                    Push();
+                    break;
+                case "that":
+                    InstruccionesASM.add("@THAT");
+                    InstruccionesASM.add("D=M");
+                    InstruccionesASM.add("@"+Contenido[2]);
+                    InstruccionesASM.add("A=D+A");
+                    InstruccionesASM.add("D=M");
+                    Push();
+                    break;
+                case "pointer":
+                {
+                    if (Contenido[2].equals("0")) {
+                        InstruccionesASM.add("@THIS");
+                        InstruccionesASM.add("D=M");
+                        Push();
+                    }
+                    else { 
+                        InstruccionesASM.add("@THIS");
+                        InstruccionesASM.add("D=M");
+                        Push();
+                    }
+                    break;
+                }
+                case "constant":
+                    InstruccionesASM.add("@"+Contenido[2]);
+                    InstruccionesASM.add("D=A");
+                    Push();
+                    break;
+                case "static":
+                    InstruccionesASM.add("@var"+Contenido[2]);
+                    InstruccionesASM.add("D=M");
+                    Push();
+                    break;
+                case "temp":
+                   InstruccionesASM.add("@R5");
+                   InstruccionesASM.add("D=A");
+                   InstruccionesASM.add("@"+Contenido[2]);
+                   InstruccionesASM.add("A=D+A");
+                   InstruccionesASM.add("D=M");
+                   Push();
+                   break;
+                default:
+                    break;
+            }
         }
     }
 
+    public void Push()
+    {
+        InstruccionesASM.add("@SP");
+        InstruccionesASM.add("A=M");
+        InstruccionesASM.add("M=D");
+        InstruccionesASM.add("@SP");
+        InstruccionesASM.add("M=M+1");
+    }
+    
+    public void Pop()
+    { 
+        InstruccionesASM.add("@R13");
+        InstruccionesASM.add("M=D");
+        InstruccionesASM.add("@SP");
+        InstruccionesASM.add("AM=M-1");
+        InstruccionesASM.add("D=M");
+        InstruccionesASM.add("@R13");
+        InstruccionesASM.add("A=M");
+        InstruccionesASM.add("M=D");
+    }
+    
     int eqs = 1;
     public void arithmetic(String word){
 
@@ -395,34 +483,34 @@ public class VM_Traductor {
         InstruccionesASM.add("@LCL");
         InstruccionesASM.add("D=M");
         InstruccionesASM.add("@R13");
-        InstruccionesASM.add("M=D");	//R13 is now FRAME
+        InstruccionesASM.add("M=D");	
         InstruccionesASM.add("D=M-5");
         InstruccionesASM.add("@R14");
-        InstruccionesASM.add("M=D\n");	//R14 stores RET
+        InstruccionesASM.add("M=D\n");	
         InstruccionesASM.add("@SP\n");
         InstruccionesASM.add("M=A\n");
         InstruccionesASM.add("D=M\n");
         InstruccionesASM.add("@ARG\n");
         InstruccionesASM.add("A=M\n");
-        InstruccionesASM.add("M=D\n");	// *ARG = pop()
+        InstruccionesASM.add("M=D\n");	
         InstruccionesASM.add("@ARG\n");
         InstruccionesASM.add("D=M\n");
         InstruccionesASM.add("@SP\n");
         InstruccionesASM.add("M=A\n");
-        InstruccionesASM.add("M=D+1\n");		//SP = ARG + 1
+        InstruccionesASM.add("M=D+1\n");		
         InstruccionesASM.add("@R13\n");
         InstruccionesASM.add("D=M\n");
-        InstruccionesASM.add("@THAT\n");	//THAT = FRAME-1
+        InstruccionesASM.add("@THAT\n");	
         InstruccionesASM.add("M=D-1\n");
         InstruccionesASM.add("@THIS\n");
-        InstruccionesASM.add("M=D-2\n");	//FRAME - 2
+        InstruccionesASM.add("M=D-2\n");	
         InstruccionesASM.add("@ARG\n");
-        InstruccionesASM.add("M=D-3\n");	//FRAME-3
+        InstruccionesASM.add("M=D-3\n");	
         InstruccionesASM.add("@LCL\n");
-        InstruccionesASM.add("M=D-4\n");	//FRAME - 4
+        InstruccionesASM.add("M=D-4\n");	
         InstruccionesASM.add("@R14\n");
         InstruccionesASM.add("D=M\n");
-        InstruccionesASM.add("@D\n");	//GOTO RET
+        InstruccionesASM.add("@D\n");	
         InstruccionesASM.add("0;JMP\n");
     }
     
